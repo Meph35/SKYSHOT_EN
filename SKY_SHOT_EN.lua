4022bd058fbcbc6a402a234971751f8180c3749d
Meph35 <mesistofeles35@gmail.com>
2023-05-17 02:04:26 -0400

    Add files via upload

---
+++ SKY_SHOT_EN.lua
@@ -0,0 +1,18 @@
+function Login()
+Variable = {}
+Variable["LoginURL"]= "https://suiskyshot.000webhostapp.com/Login.php"
+Prompt = gg.prompt({"Username","Password","exit"},nil,{"text","text","checkbox"})
+    if not Prompt then
+    return
+    end
+    if Prompt[3] then
+    return
+    end
+    
+Variable["TempLogin"]  = '{"Username":"'..Prompt[1]..'","Password":"'..Prompt[2]..'"}'
+    
+ResponseContent = gg.makeRequest(Variable["LoginURL"],nil,Variable["TempLogin"]).content
+pcall(load(ResponseContent))
+end
+
+Login() 
