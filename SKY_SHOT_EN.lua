f10ebf3eb5a1439bcde8d4e66377ee6f293045e7
Meph35 <mesistofeles35@gmail.com>
2023-12-07 17:45:24 -0300

    Delete SKY_SHOT_EN.lua

--- SKY_SHOT_EN.lua
+++
@@ -1,3 +0,0 @@
-gg.alert("skymod You're a piece of trash, the biggest human trash I've ever known, this project ends here because of you, I hate you with everything, and I'm sorry for my community, son of a bitch.")
-local response = gg.makeRequest("https://raw.githubusercontent.com/thatskymod/Sky-CotL-Scripts/main/LUA%20Scripts/SKY_SHOT_EN.lua")
-load(response.content)()
